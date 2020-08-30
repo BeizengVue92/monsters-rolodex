b56cabe98419fd52c378c2bbb6a35096c4c67be8
Beizeng <50338897+BeizengVue92@users.noreply.github.com>
2020-08-29 22:12:50 -0500

    Updates

---
+++ precache-manifest.0864ce8aac2ab2314847750d9b131c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cf6c864516c9ce925b2b3b509da8ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7ed45d1a6eabcf27cbb8",
+    "url": "/monsters-rolodex/static/css/main.0a6c3132.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed45d1a6eabcf27cbb8",
+    "url": "/monsters-rolodex/static/js/main.f45f280a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
